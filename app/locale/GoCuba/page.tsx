082b1d99627d7458a7b647a0bb116cb234e5455b
meldofficial9 <meldofficial9@gmail.com>
2025-11-04 17:08:32 -0500

    Add files via upload

---
+++ app/locale/GoCuba/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+export default function Page() {
+  return (
+    <div className="space-y-2">
+      <h1 className="text-2xl font-semibold">Gocuba</h1>
+      <p className="opacity-80">This is the GoCuba page.</p>
+    </div>
+  );
+}
