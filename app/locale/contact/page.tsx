71b7663006036761391f8ba37429e0e508b6e2c8
meldofficial9 <meldofficial9@gmail.com>
2025-11-04 19:08:46 -0500

    Delete app/locale directory

--- app/locale/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-export default function Page() {
-  return (
-    <div className="space-y-2">
-      <h1 className="text-2xl font-semibold">Contact</h1>
-      <p className="opacity-80">This is the contact page.</p>
-    </div>
-  );
-}
--- app/locale/resources/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-export default function Page() {
-  return (
-    <div className="space-y-2">
-      <h1 className="text-2xl font-semibold">Resources</h1>
-      <p className="opacity-80">This is the resources page.</p>
-    </div>
-  );
-}
