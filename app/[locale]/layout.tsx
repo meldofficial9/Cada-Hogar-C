70562805db71a247c402bad9dea7ac6b6acb06ad
meldofficial9 <meldofficial9@gmail.com>
2025-11-05 15:26:07 -0500

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,30 +1,30 @@
 import '../globals.css';
-import type {Metadata} from 'next';
 import {NextIntlClientProvider} from 'next-intl';
-import {notFound} from 'next/navigation';
-import {getMessages, getLocale} from 'next-intl/server';
+import {getMessages} from 'next-intl/server';
 
-export const metadata: Metadata = {
-  title: 'CadaHogar Cuba',
-  description: 'Each home matters.'
+export const metadata = {
+  title: 'Cada Hogar Cuba',
+  description: 'GoCuba'
 };
 
+export function generateStaticParams() {
+  return [{locale: 'en'}, {locale: 'es'}];
+}
+
 export default async function RootLayout({
-  children
+  children,
+  params: {locale}
 }: {
   children: React.ReactNode;
+  params: {locale: string};
 }) {
-  const locale = await getLocale();
-  if (!['en', 'es'].includes(locale)) {
-    notFound();
-  }
   const messages = await getMessages();
 
   return (
     <html lang={locale}>
-      <body className="min-h-screen">
+      <body>
         <NextIntlClientProvider messages={messages}>
-          <main className="max-w-5xl mx-auto p-6">{children}</main>
+          {children}
         </NextIntlClientProvider>
       </body>
     </html>
