c3015ad07ab0c97d4853c210b221652fdbd712c0
meldofficial9 <meldofficial9@gmail.com>
2026-04-13 22:12:37 -0400

    Update request.ts

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,6 +1,8 @@
 import {getRequestConfig} from 'next-intl/server';
 
-export default getRequestConfig(async ({locale}) => {
+export default getRequestConfig(async ({requestLocale}) => {
+  const locale = await requestLocale;
+
   return {
     locale,
     messages: (await import(`../messages/${locale}.json`)).default
