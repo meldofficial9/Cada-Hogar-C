9d698b3a23454bb4cfdfb9ffce2844a8af82b365
meldofficial9 <meldofficial9@gmail.com>
2026-04-13 22:14:45 -0400

    Update page.tsx

--- app/[locale]/GoCuba/page.tsx
+++ app/[locale]/GoCuba/page.tsx
@@ -1,9 +1,18 @@
-'use client';
-export default function Page() {
+import {setRequestLocale} from 'next-intl/server';
+import {useTranslations} from 'next-intl';
+
+export default function AboutPage({
+  params: {locale}
+}: {
+  params: {locale: 'en' | 'es'};
+}) {
+  setRequestLocale(locale);
+
+  const t = useTranslations('About');
+
   return (
-    <div className="space-y-2">
-      <h1 className="text-2xl font-semibold">Gocuba</h1>
-      <p className="opacity-80">This is the GoCuba page.</p>
+    <div>
+      <h1>{t('title')}</h1>
     </div>
   );
 }
