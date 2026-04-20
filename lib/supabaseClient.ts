2dc0a85c83684f683e44f65f9d2f29aa76ad665d
meldofficial9 <meldofficial9@gmail.com>
2026-04-20 17:32:28 -0400

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,10 +1,11 @@
 'use client';
-import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+import {createClient} from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase =
-  supabaseUrl && supabaseAnonKey
+  supabaseUrl && supabaseAnonKey && /^https?:\/\//.test(supabaseUrl)
     ? createClient(supabaseUrl, supabaseAnonKey)
-    : (null as any);
+    : null;
