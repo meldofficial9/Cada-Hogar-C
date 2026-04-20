0eb4bdae4c2a9489a48d16ac3e64f875e18db979
meldofficial9 <meldofficial9@gmail.com>
2026-04-20 17:26:38 -0400

    Delete lib/supabseAdmin.ts

--- lib/supabseAdmin.ts
+++
@@ -1,9 +0,0 @@
-import {createClient} from '@supabase/supabase-js';
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY as string;
-
-export const supabaseAdmin =
-  supabaseUrl && serviceRoleKey
-    ? createClient(supabaseUrl, serviceRoleKey)
-    : (null as any);
